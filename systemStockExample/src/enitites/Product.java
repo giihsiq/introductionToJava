ae12966ede5cbd9aadb2cc12f4188a25ee1353ca
Giovanna Siqueira <giihsiq.contato@gmail.com>
2025-09-03 13:09:20 -0300

    Refactor method toString()

--- systemStockExample/src/enitites/Product.java
+++ systemStockExample/src/enitites/Product.java
@@ -21,6 +21,7 @@ public class Product {
 			   + ", $ "
 			   + String.format("%.2f", price)
 			   + ", " + quantity
-			   + " units, Total: $ " + totalValueInStock();
+			   + " units, Total: $ "
+			   + totalValueInStock();
 	}
 }
